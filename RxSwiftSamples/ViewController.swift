51e1107e044c206dcfa2bd86e5427376a6f0d219
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2019-02-20 15:10:28 +0900

    RxSwiftの1つ目のサンプルを追加。

--- RxSwiftSamples/ViewController.swift
+++ RxSwiftSamples/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+
+    let label1 = UILabel()
+    let textField = UITextView()
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        label1.frame = CGRect(x: 50, y: 100, width: 300, height: 50)
+        label1.backgroundColor = UIColor.gray
+        view.addSubview(label1)
+
+        textField.frame = CGRect(x: 50, y: 250, width: 300, height: 50)
+        textField.backgroundColor = UIColor.cyan
+        textField.text = "test111"
+        view.addSubview(textField)
+
+        textField.rx.text
+            .map {"「\($0!)」"}
+            .bind(to: label1.rx.text)
+            .disposed(by: disposeBag)
     }
 
 
